b378b31ebde0009038a233a04059bac2b8462338
siddheshmhapankar2478 <siddheshmhapankar2478@gmail.com>
2025-05-18 18:17:13 +0530

    added montserat font

--- App.js
+++ App.js
@@ -6,32 +6,22 @@
  */
 
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
+import {calcSize} from './src/components/Responsive';
 
 function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text>Hello World</Text>
-    </View>
+    <Text style={styles.quicksandRegular}>
+      This text uses a quick sand font
+    </Text>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  text: {fontSize: calcSize(24), fontWeight: '600', color: 'black'},
+  quicksandRegular: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 20,
   },
 });
 
