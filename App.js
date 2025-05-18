5efe6be2483b4082011eb52d8ecc70c626a7a20b
siddheshmhapankar2478 <siddheshmhapankar2478@gmail.com>
2025-05-18 17:27:16 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
